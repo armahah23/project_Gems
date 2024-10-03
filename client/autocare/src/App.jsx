662e8544a9957f9750c1982f4e2d9acd64f7e2d9
Abdullah0928 <mnabdullah0928@gmail.com>
2024-10-03 22:25:35 +0530

    deleted

--- client/autocare/src/App.jsx
+++ client/autocare/src/App.jsx
@@ -1,23 +1,12 @@
 import "./App.css";
-// import Footer from "./Components/Footer";
-// import Home from "./Components/Home";
-// import Navbar from "./Components/Navbar";
-//import Asignup from '../src/Components/Pages/Asignup'
-import Esignup from '../src/Components/Pages/Esignup'
-//import SignupOption from './src/Components/Pages/SignupOption'
+
+
 
 function App() {
   return (
-    <>
-      {/* <Navbar />
-      <Home />
-      <Footer /> 
-      <Asignup />*/}
-      <Esignup/>
+      <>
+      
       </>
-
-
-    
   );
 }
 
