6c18c30cd8b73c41c739bdf1245d2609ea3b0e2d
armahah23 <armahemp.off@gmail.com>
2024-11-05 18:26:34 +0530

    refactor: standardize import statements and improve JSX formatting in ADashboard component

--- client/src/Pages/adminDashboard/ADashboard.jsx
+++ client/src/Pages/adminDashboard/ADashboard.jsx
@@ -1,18 +1,20 @@
-import Widget from './adminComponent/widget';
-import BookingsEarningsGraph from './adminComponent/BookingsEarningsGraph';
-import RatingsWidget from './adminComponent/RatingsWidget';
+import Widget from "./adminComponent/Widget";
+import BookingsEarningsGraph from "./adminComponent/BookingsEarningsGraph";
+import RatingsWidget from "./adminComponent/RatingsWidget";
 import "./ADashboard.css"; // Add your own styling
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const ADashboard = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <div className={`Adashboard ${isDarkMode ? 'dark' : ''}`}>
-  <h2 className="m-6 text-[56px] font-extrabold uppercase
-    text-primary-color dark:text-white">
-    Dashboard
-  </h2>
+    <div className={`Adashboard ${isDarkMode ? "dark" : ""}`}>
+      <h2
+        className="m-6 text-[56px] font-extrabold uppercase
+    text-primary-color dark:text-white"
+      >
+        Dashboard
+      </h2>
 
       <div className="flex p-[20px] w-[100%] gap-[20px]">
         <Widget type="user" />
