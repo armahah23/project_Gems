907715cf177be2cfdb5be8652fa894c25ad60ed4
armahah23 <armahemp.off@gmail.com>
2024-11-05 18:42:41 +0530

    fix: set default preview port to 4173 in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: parseInt(import.meta.env.VITE_PORT) || 4173,
+    port: 4173,
   },
 });
