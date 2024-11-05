d354862a514cb439fb9c5e5b2f04c0c47a3631a2
armahah23 <armahemp.off@gmail.com>
2024-11-05 18:38:37 +0530

    feat: add .env file for VITE_PORT configuration and update vite.config.js for preview port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: parseInt(import.meta.env.VITE_PORT) || 4173,
+  },
+});
